fb463e35185afaa5e7ce2619020f8555854d8a55
opa334 <opa334@users.noreply.github.com>
2021-12-08 21:53:20 +0100

    Add Hebrew localization thanks to @guezomri

--- update_localizations.sh
+++ update_localizations.sh
@@ -1,2 +1,3 @@
 localisort -t ./layout/Library/Application\ Support/Choicy.bundle/en.lproj/Localizable.strings -i ./layout/Library/Application\ Support/Choicy.bundle/de.lproj/Localizable.strings -r
 localisort -t ./layout/Library/Application\ Support/Choicy.bundle/en.lproj/Localizable.strings -i ./layout/Library/Application\ Support/Choicy.bundle/el.lproj/Localizable.strings -r
+localisort -t ./layout/Library/Application\ Support/Choicy.bundle/en.lproj/Localizable.strings -i ./layout/Library/Application\ Support/Choicy.bundle/he.lproj/Localizable.strings -r
